d7e4f861b63e09c5ffe9c3d8339100d0c7555167
jadsonsts <jadson.s.carneiro@gmail.com>
2023-02-06 12:04:24 +1300

    first commit

---
+++ BudgetManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudgetManager
+//
+//  Created by Jadson on 6/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
