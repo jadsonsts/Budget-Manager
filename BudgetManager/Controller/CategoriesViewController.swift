ee90cbe8217da575666a6581ace54dd94b8bb4a4
jadsonsts <jadson.s.carneiro@gmail.com>
2023-02-08 23:05:22 +1300

    add project icon, add content(view) of login with third party, manual sign up and log in

---
+++ BudgetManager/Controller/CategoriesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoriesViewController.swift
+//  BudgetManager
+//
+//  Created by Jadson on 8/02/23.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BudgetManager/SupportFiles/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  BudgetManager
+//
+//  Created by Jadson on 8/02/23.
+//
+
+import Foundation
