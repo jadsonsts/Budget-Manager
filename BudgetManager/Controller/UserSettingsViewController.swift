33e69d408165869a5826c9be4aecadf9ff543499
jadsonsts <jadson.s.carneiro@gmail.com>
2024-02-11 02:02:00 +1300

    display user data on user Settings section

--- BudgetManager/Controller/UserSettingsViewController.swift
+++ BudgetManager/Controller/UserSettingsViewController.swift
@@ -22,6 +22,7 @@ class UserSettingsViewController: UIViewController {
     @IBOutlet weak var updateProfileButton: CustomButton!
     @IBOutlet weak var disclaimerLabel: UILabel!
     
+    var userDetails: Customer?
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -30,13 +31,58 @@ class UserSettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadUserData()
+        loadProfilePicture()
         updateProfileButton.isHidden = true
         createKeyboardDoneButton()
-        disclaimerLabel.text = "⚠️ User profile will be available for editing in the next update ⚠️"
+        disclaimerLabel.text = "⚠️ User profile will be available for editing in the future updates ⚠️"
+        userProfilePicture.layer.cornerRadius = 40
+        userProfilePicture.clipsToBounds = true
 
     }
     
+    func loadLabels() {
+        firstNameTextField.text = userDetails?.name
+        lastNameTextField.text = userDetails?.familyName
+        emailTextField.text = userDetails?.email
+        phoneNumberTextField.text = userDetails?.phone
+        
+    }
     
+    func loadProfilePicture() {
+        DataController.shared.loadPhoto { [weak self] customerImage in
+            //safeImage is the check from userDefaults, if there's no image, the app will check on firebase
+            if let safeImage = customerImage {
+                self?.userProfilePicture.image = safeImage
+            } else {
+                DataController.shared.downloadPhotoFromFirebase { [weak self] userProfilePicture in
+                    self?.userProfilePicture.image = userProfilePicture
+                }
+            }
+        }
+    }
+    
+    func loadUserData()  {
+        ProgressHUD.show()
+        guard let userID = Auth.auth().currentUser?.uid else { return }
+        DataController.shared.fetchCustomer(userID) { [weak self] customer in
+            self?.userDetails = customer
+            self?.loadLabels()
+            ProgressHUD.dismiss()
+        } onError: { errorMessage in
+            ProgressHUD.showFailed(errorMessage)
+        }
+        
+    }
+
+    @IBAction func updateProfileButtonPressed(_ sender: CustomButton) {
+    }
+
+}
+
+//MARK: - Keyboard Settings
+extension UserSettingsViewController {
     func createKeyboardDoneButton() {
         let textFields: [UITextField] = [lastNameTextField, firstNameTextField, emailTextField, phoneNumberTextField, passwordTextField, confirmPasswordTextField]
         
@@ -46,9 +92,4 @@ class UserSettingsViewController: UIViewController {
     @objc func doneButtonAction(){
         view.endEditing(true)
     }
-    
-    @IBAction func updateProfileButtonPressed(_ sender: CustomButton) {
-    }
-
-
 }
